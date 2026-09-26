92ef0739871a48bc220db8251cd073fa9495f7a7
Himmat Magar <himmatmagar007@gmail.com>
2026-09-26 13:41:52 +0545

    implementing GPT model

--- main.py
+++ main.py
@@ -42,39 +42,45 @@ def create_dataloader_v1(txt, batch_size=4, max_length=256, stride=128, shuffle=
     return dataloder
 
 
-class CausalSelfAttention(nn.Module):
-    def __init__(self, in_d, out_d, dropout, context_length, qkv_bias = False):
+class MultiHeadAttention(nn.Module):
+    def __init__(self, in_d, out_d, context_length, dropout, n_heads, qkv_bias = False):
         super().__init__()
-        self.w_query = torch.nn.Linear(in_d, out_d, bias=qkv_bias)
-        self.w_key = torch.nn.Linear(in_d, out_d, bias=qkv_bias)
-        self.w_value = torch.nn.Linear(in_d, out_d, bias=qkv_bias)
-        self.dropout = torch.nn.Dropout(dropout)
+        assert(out_d % n_heads == 0)
+
+        self.d_out = out_d
+        self.n_heads = n_heads
+        self.head_dim = out_d // n_heads
+
+        self.w_query = nn.Linear(in_d, out_d, bias=qkv_bias)
+        self.w_key = nn.Linear(in_d, out_d, bias=qkv_bias)
+        self.w_value = nn.Linear(in_d, out_d, bias=qkv_bias)
+        self.out_proj = nn.Linear(out_d, out_d)
+        self.dropout = nn.Dropout(dropout)
         self.register_buffer("mask", torch.triu(torch.ones(context_length, context_length), diagonal=1))
 
-
     def forward(self, inputs):
-        b, num_token, d_in = inputs.shape
+        b, num_tokens, d_in = inputs.shape
 
         query = self.w_query(inputs)
-        key = self.w_key(inputs)
-        value = self.w_value(inputs)
+        keys = self.w_key(inputs)
+        values = self.w_value(inputs)
 
-        attn_scores = query @ key.transpose(1, 2)
-        attn_scores.masked_fill_(self.mask.bool()[:num_token, :num_token], -torch.inf)
-        attn_weight = torch.softmax(attn_scores / key.shape[-1]**0.5, dim=1)
+        keys = keys.view(b, num_tokens, self.n_heads, self.head_dim)
+        values = values.view(b, num_tokens, self.n_heads, self.head_dim)
+        queries = query.view(b, num_tokens, self.n_heads, self.head_dim)
+
+        keys = keys.transpose(1, 2)
+        values = values.transpose(1, 2)
+        queries = queries.transpose(1, 2)
+
+        attn_score = queries @ keys.transpose(2, 3)
+        attn_score.masked_fill_(self.mask.bool()[:num_tokens, :num_tokens], -torch.inf)
+        attn_weight = torch.softmax(attn_score / keys.shape[-1] ** 0.5, dim=-1)
         attn_weight = self.dropout(attn_weight)
 
-        context_vec = attn_weight @ value
+        context_vec = (attn_weight @ values).transpose(1, 2)
+
+        context_vec = context_vec.reshape(b, num_tokens, self.d_out)
+        context_vec = self.out_proj(context_vec)
+
         return context_vec
-
-
-class MultiHeadAttention(nn.Module):
-
-    def __init__(self, in_d, out_d, dropout, context_len, head, qkv_bias=False):
-        super().__init__()
-        self.heads = torch.nn.ModuleList(
-            [CausalSelfAttention(in_d, out_d, dropout, context_len, qkv_bias) for _ in range(head)]
-        )
-
-    def forward(self, x):
-        return torch.cat([head(x) for head in self.heads], dim=-1)
