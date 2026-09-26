596dd66792c0304e70931006ffc87bc32bfd6a9e
Himmat Magar <himmatmagar007@gmail.com>
2026-09-26 14:56:29 +0545

    implemention essential part

--- main.py
+++ main.py
@@ -84,3 +84,108 @@ class MultiHeadAttention(nn.Module):
         context_vec = self.out_proj(context_vec)
 
         return context_vec
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True)
+        normed = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * normed + self.shift
+
+
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0/torch.pi)) * (x + 0.044715 * torch.pow(x, 3))
+        ))
+
+
+class FeedFrowardNetwork(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(cfg['emb_dim'], 4 * cfg['emb_dim']),
+            GELU(),
+            nn.Linear(4 * cfg['emb_dim'], cfg['emb_dim'])
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.att = MultiHeadAttention(
+            in_d=cfg['emb_dim'],
+            out_d=cfg['emb_dim'],
+            dropout=cfg['drop_rate'],
+            context_length=cfg['context_length'],
+            n_heads=cfg['n_heads'],
+            qkv_bias=cfg['qkv_bias']
+        )
+        self.ff = FeedFrowardNetwork(cfg)
+        self.norm1 = LayerNorm(cfg['emb_dim'])
+        self.norm2 = LayerNorm(cfg['emb_dim'])
+        self.drop_shorcuts = nn.Dropout(cfg['drop_rate'])
+
+    def forward(self, x):
+        shortcuts = x
+        x = self.norm1(x)
+        x = self.att(x)
+        x = self.drop_shorcuts(x)
+        x = x + shortcuts
+
+        shortcuts = x
+        x = self.norm1(x)
+        x = self.ff(x)
+        x = self.drop_shorcuts(x)
+        x = x + shortcuts
+
+        return x
+
+
+class GPTModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.tok_emb = nn.Embedding(cfg['vocab_size'], cfg['emb_dim'])
+        self.pos_emb = nn.Embedding(cfg['context_length'], cfg['emb_dim'])
+        self.drop_emb = nn.Dropout(cfg['drop_rate'])
+
+        self.t_block = nn.Sequential(
+            *[TransformerBlock(cfg) for _ in range(cfg['n_layers'])]
+        )
+        self.final_norm = LayerNorm(cfg["emb_dim"])
+        self.out_head = nn.Linear(cfg['emb_dim'], cfg['vocab_size'], bias=False)
+
+    def forward(self, in_idx):
+        _, seq_len = in_idx.shape
+        tok_emb = self.tok_emb(in_idx)
+        pos_emb = self.pos_emb(torch.arange(seq_len, device=in_idx.device))
+        x = tok_emb + pos_emb
+        x = self.drop_emb(x)
+        x = self.t_block(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
+
+def GenerateSimpleText(model, idx, max_num_token, context_size):
+    for _ in range(max_num_token):
+        idx_contd = idx[:, -context_size:]
+
+        with torch.no_grad():
+            logits = model(idx_contd)
+        logits = logits[:, -1, :]
+        prob = torch.softmax(logits, dim=-1)
+        idx_next = torch.argmax(prob, dim=-1, keepdim=True)
+        idx = torch.cat((idx, idx_next), dim=1)
+    return idx
